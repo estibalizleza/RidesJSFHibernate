d7f5544d45bbfe4e5158262709c5515d0010c20f
estibalizleza <eleza002@ikasle.ehu.eus>
2024-12-07 14:22:39 +0100

    igo

---
+++ RidesJSF/src/main/java/businessLogic/BLFacadeImplementation.java
@@ -0,0 +1,112 @@
+package businessLogic;
+import java.util.Date;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import dataAccess.DataAccess;
+import domain.Ride;
+import domain.Driver;
+import exceptions.RideMustBeLaterThanTodayException;
+import exceptions.RideAlreadyExistException;
+
+/**
+ * It implements the business logic as a web service.
+ */
+public class BLFacadeImplementation  implements BLFacade {
+	DataAccess dbManager;
+
+	public BLFacadeImplementation()  {		
+		System.out.println("Creating BLFacadeImplementation instance");
+		
+		
+		    dbManager=new DataAccess();
+		    
+		//dbManager.close();
+
+		
+	}
+	
+    public BLFacadeImplementation(DataAccess da)  {
+		
+		System.out.println("Creating BLFacadeImplementation instance with DataAccess parameter");
+		dbManager=da;		
+	}
+    
+    
+    /**
+     * {@inheritDoc}
+     */
+    public List<String> getDepartCities(){
+    	dbManager.open();	
+		
+		 List<String> departLocations=dbManager.getDepartCities();		
+
+		dbManager.close();
+		
+		return departLocations;
+    	
+    }
+    /**
+     * {@inheritDoc}
+     */
+	public List<String> getDestinationCities(String from){
+		dbManager.open();	
+		
+		 List<String> targetCities=dbManager.getArrivalCities(from);		
+
+		dbManager.close();
+		
+		return targetCities;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+   public Ride createRide( String from, String to, Date date, int nPlaces, float price, String driverEmail ) throws RideMustBeLaterThanTodayException, RideAlreadyExistException{
+	   
+		dbManager.open();
+		Ride ride=dbManager.createRide(from, to, date, nPlaces, price, driverEmail);		
+		dbManager.close();
+		return ride;
+   };
+	
+   /**
+    * {@inheritDoc}
+    */
+	public List<Ride> getRides(String from, String to, Date date){
+		dbManager.open();
+		List<Ride>  rides=dbManager.getRides(from, to, date);
+		dbManager.close();
+		return rides;
+	}
+
+    
+	/**
+	 * {@inheritDoc}
+	 */
+	public List<Date> getThisMonthDatesWithRides(String from, String to, Date date){
+		dbManager.open();
+		List<Date>  dates=dbManager.getThisMonthDatesWithRides(from, to, date);
+		dbManager.close();
+		return dates;
+	}
+	
+	
+	public void close() {
+		DataAccess dB4oManager=new DataAccess();
+
+		dB4oManager.close();
+
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	 public void initializeBD(){
+    	dbManager.open();
+		dbManager.initializeDB();
+		dbManager.close();
+	}
+
+}
+
---
+++ RidesJSF/src/main/java/rides/bean/MainBean.java
@@ -0,0 +1,13 @@
+package rides.bean;
+
+public class MainBean {
+
+
+	public String goToQueryRides() {
+		return "QueryRides";
+	}
+
+	public String goToCreateRide() {
+		return "CreateRide";
+	}
+}
